6165dae95ae20ff31591f0655b65f24f0eb05b1b
mihaioancea00798@gmail.com <mihaioancea00798@gmail.com>
2022-08-17 12:35:56 +0000

    adjusting the task

---
+++ Includes/databases.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC CREATE DATABASE IF NOT EXISTS odap_bronze;
+# MAGIC CREATE DATABASE IF NOT EXISTS silver;
+
+# COMMAND ----------
+
+import os
+print(os.getcwd())
+
+# COMMAND ----------
+
+
